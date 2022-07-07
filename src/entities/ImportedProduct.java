6fff964dfc783b33c891f6d6b7ee9ee639e3dd8b
Lucas Ferraz <lferraz802@gmail.com>
2022-07-07 17:16:48 -0300

    Update files

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class ImportedProduct extends Product{
+	
+	private double customsFee;
+	
+	public ImportedProduct() {
+		
+	}
+
+	public ImportedProduct(String name, Double price, double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(double customsFee) {
+		this.customsFee = customsFee;
+	}
+
+	public Double totalPrice() {
+		return getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ " R$ " 
+				+ String.format("%.2f", totalPrice())
+				+ "(Customs fee: R$ "
+				+ String.format("%.2f", customsFee)
+				+")";
+	}
+}
